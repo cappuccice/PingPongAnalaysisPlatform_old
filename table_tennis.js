301ec82cb8c87a9aa3e870fab4f665a4e52b27b9
K15112 <k15112kk@aitech.ac.jp>
2017-08-17 15:01:13 +0900

    テーブルの作成完了

--- table_tennis.js
+++ table_tennis.js
@@ -1,10 +1,64 @@
-function nextBall(){
+//仮
+var player = "選手A";
+var net = "レット";
+var service = "ミドル";
+var higher = "低い";
+var form = "チキータ";
+var course = "フォア前";
 
-}
+var receive ="フォア";
+var side = "フロント";
 
-function nextService(){
 
-}
+var larryCount = 2;
+var headCount = 2;
+
+
+
+
+$('#nextBall').on('click', function(){
+  larryCount = larryCount + 1;
+  $('tr:last').append(
+      $('<td>').append(receive),
+      $('<td>').append(net),
+      $('<td>').append(form),
+      $('<td>').append(course),
+  )
+  if(headCount < larryCount){
+    headCount = larryCount;
+    var li = "<th>" + headCount + "打球目</th>"
+    $('#oppai').append(
+      $('<th colspan="4">').append(li)
+    )
+    $('#oppai2').append(
+      $('<th>').append("<th>打球位置</th>"),
+      $('<th>').append("<th>ラケット面</th>"),
+      $('<th>').append("<th>打法</th>"),
+      $('<th>').append("<th>コース</th>"),
+    )
+  }
+});
+
+$('#nextService').on('click', function() {
+  $('#table_body').append(
+    $('<tr>').append(
+      $('<td>').append(player),
+      $('<td>').append(net),
+      $('<td>').append(service),
+      $('<td>').append(higher),
+      $('<td>').append(form),
+      $('<td>').append(course),
+      $('<td>').append(receive),
+      $('<td>').append(side),
+      $('<td>').append(form),
+      $('<td>').append(course)
+    )
+  )
+  larryCount = 2;
+});
+
+
+
 
 function movieChange(){
   //inputフィールドのテキスト内容を文字列として取得
@@ -18,7 +72,7 @@ function movieChange(){
   console.log(youtube_url);
 
   //埋め込みリンクをidがmovieのsrc(ソース)に適応
-  document.getElementById("movie").src = youtube_url;
+  document.getElementById("movie_src").src = youtube_url;
 }
 
 //渡された文字列を分割する関数
@@ -35,3 +89,11 @@ function splitString (stringToSplit,separator) {
   //分割した文字列の配列5番目にurlが入っているのでリターン
   return arrayOfStrings[5];
 }
+
+// 作成するテーブルの定義
+ var r_end = 5;  // 行数
+ var c_end = 4;  // 列数
+
+/*------------------------------------------------
+ jQuery で動的にテーブルを作成する
+--------------------------------------------------*/
